d37d7d7ee6d836fee5618f5abf7e2c7935c77643
hjj <1689692283@qq.com>
2019-04-18 10:23:32 +0800

    cmfz

--- src/main/java/com/baizhi/controller/BannerController.java
+++ src/main/java/com/baizhi/controller/BannerController.java
@@ -8,44 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
