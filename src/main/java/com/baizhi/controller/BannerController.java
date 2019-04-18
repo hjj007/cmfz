a379af6760e43b267a9b26b8e3042eab4219fed8
hjj007 <49516835+hjj007@users.noreply.github.com>
2019-04-18 10:21:50 +0800

    Update BannerController.java

--- src/main/java/com/baizhi/controller/BannerController.java
+++ src/main/java/com/baizhi/controller/BannerController.java
@@ -8,6 +8,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
